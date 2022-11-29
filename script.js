fe298c3bb2e4fef1f8fb801a557bc1d55cc26be8
Flander404 <109587664+Flander404@users.noreply.github.com>
2022-11-29 20:53:26 +0300

    fdxv
    dfsddva

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
-var toggle = document.querySelector('.toggle')
+var toggle = document.querySelector('#check')
 var menu = document.querySelector('.nav-menu')
 
+toggle.addEventListener("click", () => {
+    toggle.classList.toggle("active")
+    menu.classList.toggle("active")
+})
