86285ed9325e98a8716ce37ab5df3548ab1fa986
Javlonbek <nodirbeklider12@gmail.com>
2022-11-30 18:50:38 +0500

    fgbjmis
    qwrfsdvsd

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var toggle = document.querySelector('#check')
+var toggle = document.querySelector('.menu')
 var menu = document.querySelector('.nav-menu')
 
 toggle.addEventListener("click", () => {
